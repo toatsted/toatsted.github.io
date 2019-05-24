868efd584c542ab9e33e68c76b115de54c37e825
Sam Hicks <samueljacksonhicks@gmail.com>
2019-05-24 02:40:40 -0500

    fixed my cool workaround that I thought would be cool but ended up messing up firefox :(

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 #portfolio?
 
-# BIG ERROR | 
-  when using file:// protocol on firefox the navbar fails to load and it throws a `XML Parsing Error: junk after document element` 
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,3 @@
-$.get("assets/components/nav.html", function(data) {
-  $("#nav-placeholder").replaceWith(data);
-});
-
 $(document).ready(function() {
   // init sidenav
   $(".sidenav").sidenav();
